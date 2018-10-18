f9cc6d82e1885729432bd4bd825489e466fc616b
Forrest Walker <me@forrestwalker.net>
2018-10-18 06:27:48 -0500

    Added Social Login

--- components/Form/SocialIcons.js
+++ components/Form/SocialIcons.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import stylesheet from "../../Containers/RegisterContainer/index.css";
-
+const GOOGLE_LOGIN = "https://forrestwalker.me/api/o1/auth/google";
+const FACEBOOK_LOGIN = "https://forrestwalker.me/api/o1/auth/facebook";
+const TWITTER_LOGIN = "https://forrestwalker.me/api/o1/auth/twitter";
 const SocialIcons = () => {
-  return <div className={stylesheet["social--icons"]}>
-    <img alt="Facebook Login" src={"../../static/icons/facebook.svg"} />
-    <img alt="Twitter Login" src={"../../static/icons/twitter.svg"} />
-    <img alt="Google Login" src={"../../static/icons/google.svg"} />
-  </div>;
+  return (
+    <div className={stylesheet["social--icons"]}>
+      <a href={FACEBOOK_LOGIN}>
+        <img alt="Facebook Login" src={"../../static/icons/facebook.svg"} />
+      </a>
+      <a href={TWITTER_LOGIN}>
+        <img alt="Twitter Login" src={"../../static/icons/twitter.svg"} />
+      </a>
+      <a href={GOOGLE_LOGIN}>
+        <img alt="Google Login" src={"../../static/icons/google.svg"} />
+      </a>
+    </div>
+  );
 };
 export default SocialIcons;
