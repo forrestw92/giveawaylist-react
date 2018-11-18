6ae05dbff687d4178722cbe239bbbd5479c9d4b8
Forrest Walker <me@forrestwalker.net>
2018-11-18 15:59:03 -0600

    Removed Google Font

--- components/head.js
+++ components/head.js
@@ -20,10 +20,6 @@ const Head = props => (
       <link rel="apple-touch-icon" href="/static/touch-icon.png" />
       <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
       <link rel="icon" href="/static/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/css?family=Lato"
-        rel="stylesheet"
-      />
       <meta property="og:url" content={props.url || defaultOGURL} />
       <meta property="og:title" content={props.title || ""} />
       <meta
