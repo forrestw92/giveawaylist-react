6f6dc45e8d0c874c79a1178819b779003f583eb7
Forrest Walker <me@forrestwalker.net>
2018-11-18 16:36:36 -0600

    Added Image Placeholder

--- components/GiveawayCard/CardImage.js
+++ components/GiveawayCard/CardImage.js
@@ -2,10 +2,12 @@ import React from "react";
 import stylesheet from "./index.css";
 import LazyLoad from "react-lazyload";
 import { string } from "prop-types";
-
+const PlaceHolder = () => {
+  return <div className={stylesheet.image} />;
+};
 const CardImage = props => {
   return (
-    <LazyLoad height={160} once>
+    <LazyLoad height={160} placeholder={<PlaceHolder />} once>
       <img
         src={props.picture.replace(".jpg", ".SR160,160.jpg")}
         alt={"Giveaway Picture"}
