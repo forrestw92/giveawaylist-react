c895edc9bd30e205ea5a49689d0372f0199fe840
Forrest Walker <me@forrestwalker.net>
2019-04-14 16:30:55 -0500

    Added open sans font

--- components/head.js
+++ components/head.js
@@ -20,6 +20,10 @@ const Head = props => (
       <link rel="apple-touch-icon" href="/static/touch-icon.png" />
       <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
       <link rel="icon" href="/static/favicon.ico" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans"
+        rel="stylesheet"
+      />
       <meta property="og:url" content={props.url || defaultOGURL} />
       <meta property="og:title" content={props.title || ""} />
       <meta
