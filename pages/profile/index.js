533f41c3f6753ea7f1f520f384f57e698516d5ed
Forrest Walker <me@forrestwalker.net>
2019-04-26 18:25:38 -0500

    Working on profile page layout

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,12 +1,144 @@
 import React from "react";
 import Head from "../../components/head";
 
+import stylesheet from "./index.css";
+import Form from "../../components/Form";
+import CheckBox from "../../components/CheckBox";
+import Button from "../../components/Button";
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "",
+      newPassword: "",
+      errorPassword: false,
+      errorNewPassword: false,
+      endingGiveaways: false
+    };
+  }
+
+  static async getInitialProps() {
+    //TODO Handle query.message from confirm link
+    return {};
+  }
+  _onChange = (e, name) => {
+    const inputVal = e.target.value;
+    if (name === "password") {
+      this.setState({ password: inputVal });
+    }
+    if (name === "newPassword") {
+      this.setState({ newPassword: inputVal });
+    }
+    if (name === "endingGiveaways") {
+      this.setState({ endingGiveaways: !this.state.endingGiveaways }, () => {
+        this.handleSubscriptions();
+      });
+    }
+  };
+  handlePasswordChange = () => {
+    //TODO Change password
+  };
+  handleDeleteAccount = () => {
+    //TODO Handle delete account
+  };
+  handleSubscriptions = () => {
+    //TODO Handle email subscriptions.
+  };
   render() {
+    const inputs = [
+      {
+        label: "Current Password",
+        value: this.state.password,
+        type: "password",
+        id: "password",
+        name: "password",
+        autoComplete: "off",
+        hasError: this.state.errorPassword
+      },
+      {
+        label: "New Password",
+        value: this.state.newPassword,
+        type: "password",
+        id: "newPassword",
+        name: "newPassword",
+        autoComplete: "off",
+        hasError: this.state.errorNewPassword
+      }
+    ];
     return (
       <React.Fragment>
         <Head title="Amazon Giveaway List - Profile" />
-        Profile Page
+        <div className={"profile"}>
+          <aside className={"sticky"}>
+            <nav role={"navigation"} className={"profile--nav"}>
+              <ul className={"nav--items"}>
+                <li className={"nav--item active"}>
+                  <a href="#stats">Profile</a>
+                </li>
+                <li className={"nav--item"}>
+                  <a href="#changePassword">Change Password</a>
+                </li>
+                <li className={"nav--item"}>
+                  <a href="#subscriptions">Subscriptions</a>
+                </li>
+                <li className="nav--item">
+                  <a href="#deleteAccount">Delete Account</a>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+          <main className={"content"}>
+            <div className="section" id={"stats"}>
+              <h1 className="title">Stats</h1>
+              <div className="stats">
+                You have been a member since {new Date().toLocaleString()}
+              </div>
+              <div className="stats">
+                You have views 900 giveaways in past 7 days
+              </div>
+              <div className="stats">
+                You have saved 9 giveaways in past 7 days
+              </div>
+            </div>
+            <div className="section" id="changePassword">
+              <Form
+                _onChange={this._onChange}
+                title={"Change Password"}
+                inputs={inputs}
+                socialLogin={false}
+              >
+                <Button
+                  _onClick={this.handlePasswordChange}
+                  label={"Confirm"}
+                  className={"login"}
+                  type={"button"}
+                />
+              </Form>
+            </div>
+            <div className="section" id="subscriptions">
+              <Form _onChange={this._onChange} title={"Subscriptions"}>
+                <CheckBox
+                  id={"endingGiveaways"}
+                  name={"endingGiveaways"}
+                  label={"Daily Ending Giveaways"}
+                  checked={this.state.endingGiveaways}
+                  _onChange={this._onChange}
+                />
+              </Form>
+            </div>
+            <div className="section" id="deleteAccount">
+              <Form _onChange={this._onChange} title={"Delete Account"}>
+                <Button
+                  _onClick={this.handleDeleteAccount}
+                  label={"Delete Account"}
+                  className={"danger"}
+                  type={"button"}
+                />
+              </Form>
+            </div>
+          </main>
+        </div>
+        <style jsx>{stylesheet}</style>
       </React.Fragment>
     );
   }
