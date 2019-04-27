fe67cb4f6a4f18a80952719ba5e0b0a54d0c4201
Forrest Walker <me@forrestwalker.net>
2019-04-26 19:39:17 -0500

    Terms Of Service and Privacy Policy Placeholder

---
+++ pages/privacy/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "../../components/head";
+
+class Privacy extends React.PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <Head title="Privacy Policy - Amazon Giveaway List" />
+        <div className={"content"}>Privacy Policy</div>
+      </React.Fragment>
+    );
+  }
+}
+export default Privacy;
---
+++ pages/tos/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "../../components/head";
+
+class Terms extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Head title="Terms Of Service - Amazon Giveaway List" />
+        <main className={"content"}>Terms of Service</main>
+      </React.Fragment>
+    );
+  }
+}
+export default Terms;
