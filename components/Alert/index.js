5c19be0c2b775c7bcda57f56047e962d52c0a729
Forrest Walker <me@forrestwalker.net>
2019-04-27 06:34:32 -0500

    Allow alert to fade out/in

--- components/Alert/index.js
+++ components/Alert/index.js
@@ -8,32 +8,38 @@ class Alert extends React.Component {
       forceHide: false
     };
   }
-
-  _startTimer = () => {
-    const { ttl, onDeath } = this.props;
+  _startDeathTimer = () => {
+    const { onDeath, transitionTime } = this.props;
     setTimeout(() => {
-      this.setState({ forceHide: true }, () => onDeath());
+      onDeath();
+    }, transitionTime * 1000);
+  };
+  _startTTLTimer = () => {
+    const { ttl } = this.props;
+    setTimeout(() => {
+      this.setState({ forceHide: true }, () => this._startDeathTimer());
     }, ttl * 1000);
   };
   componentDidMount() {
     const { show } = this.props;
     if (!show) return;
-    this._startTimer();
+    this._startTTLTimer();
   }
   componentDidUpdate(prevProps) {
     const { show } = this.props;
     if (show !== prevProps.show) {
-      this._startTimer();
+      this._startTTLTimer();
     }
   }
 
   render() {
-    const { alertType, children, show } = this.props;
+    const { alertType, children, show, transitionTime } = this.props;
     return (
       <div
         className={`alert ${alertType ? alertType : "primary"} ${
           show && !this.state.forceHide ? "" : "hide"
         }`}
+        style={{ transition: `opacity ${transitionTime}s linear` }}
         role="alert"
         aria-atomic="true"
       >
@@ -43,9 +49,13 @@ class Alert extends React.Component {
     );
   }
 }
-
+Alert.defaultProps = {
+  transitionTime: 0.33,
+  ttl: 1.5
+};
 Alert.propTypes = {
   ttl: number,
+  transitionTime: number,
   alertType: string,
   show: bool.isRequired,
   onDeath: func.isRequired,
