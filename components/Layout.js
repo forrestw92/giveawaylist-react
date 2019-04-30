b9effe014e3fe137f51a706becc1b3d0ad4d9b82
Forrest Walker <me@forrestwalker.net>
2019-04-29 21:47:13 -0500

    Fixed setting correct pageId

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter } from "next/router";
+import Router, { withRouter } from "next/router";
 import { connect } from "react-redux";
 import { node, func, object } from "prop-types";
 import { setPageId } from "../Redux/actions/navActions";
@@ -9,15 +9,12 @@ class Layout extends React.Component {
   componentDidMount() {
     const { giveawayToken } = parseCookies();
     setBearer(giveawayToken);
-  }
-  componentDidUpdate(prevProps) {
-    const { query, pathname } = this.props.router;
-    if (
-      query.pageId !== prevProps.router.query.pageId ||
-      pathname !== prevProps.router.pathname
-    ) {
-      this.props.setPageId(parseInt(query.pageId) || 1);
-    }
+    const handleRouteChange = url => {
+      const pageId =
+        url.includes("pageId") && parseInt(url.replace("/?pageId=", ""));
+      this.props.setPageId(pageId || 1);
+    };
+    Router.events.on("routeChangeStart", handleRouteChange);
   }
 
   render() {
