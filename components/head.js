3c5d4eae220641ff0740f586792fad32e788b9e2
Forrest Walker <me@forrestwalker.net>
2019-04-29 21:19:07 -0500

    Updated icons

--- components/head.js
+++ components/head.js
@@ -2,9 +2,10 @@ import React from "react";
 import NextHead from "next/head";
 import { string } from "prop-types";
 
-const defaultDescription = "";
-const defaultOGURL = "";
-const defaultOGImage = "";
+const defaultDescription =
+  "Listing all open Amazon Giveaways with Sort,Search And Filter Options.";
+const defaultOGURL = "https://test.giveawaylist.com";
+const defaultOGImage = "/static/og-image.png";
 
 const Head = props => (
   <React.Fragment>
@@ -17,12 +18,93 @@ const Head = props => (
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="manifest" href="/static/manifest.json" />
-      <link rel="icon" href="/static/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/static/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/static/apple-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/static/apple-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/static/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/static/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/static/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/static/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/static/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/static/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/static/android-icon-256x256.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/static/android-icon-512x512.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/static/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicon-16x16.png"
+      />
+
+      <meta name="theme-color" content="#1d3557" />
       <link
         href="https://fonts.googleapis.com/css?family=Open+Sans"
         rel="stylesheet"
       />
-      <link href="/static/global.css" rel="stylesheet" />
       <meta property="og:url" content={props.url || defaultOGURL} />
       <meta property="og:title" content={props.title || ""} />
       <meta
